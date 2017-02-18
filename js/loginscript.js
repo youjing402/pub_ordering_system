55a65dbdcfe476cdf0914e5b9d12df6a18336b9a
smurph7 <sarahnmurphy93@gmail.com>
2017-02-18 12:20:39 +0100

    Add files via upload

---
+++ js/loginscript.js
@@ -0,0 +1,58 @@
+
+// Get the element
+var modal = document.getElementById('modalLogin');
+var loginbtn = document.getElementById('loginbtn');
+var closex = document.getElementById('closex');
+var cancelbtn = document.getElementById('cancelbtn');
+var loginform = document.getElementById('loginform');
+var username = document.getElementById('username');
+var password = document.getElementById('password');
+
+//Open the modal on click of login button
+loginbtn.onclick = function(event) {
+	if(event.target == loginbtn) {
+		modal.style.display = "block";
+	}
+}
+
+//Close the modal when user clicks x
+closex.onclick = function(event) {
+	if(event.target == closex) {
+        modal.style.display = "none";
+	}
+}
+
+//Close the modal when user clicks close
+cancelbtn.onclick = function(event) {
+	if(event.target == cancelbtn) {
+        modal.style.display = "none";
+	}
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+//Validate the user's login details incase required attribute fails
+function validateForm() {
+	if(username==null || username=="") {
+		alert("Please enter your username");
+		return false;
+	}
+	
+	if(password==null || password=="") {
+		alert("Please enter your password");
+		return false;
+	}
+	
+	else {
+		var action = "iou_get";
+		return true;
+	}
+}
+
+
+
