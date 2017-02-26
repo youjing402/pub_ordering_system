0ce98f0ed208426f6c388e340288cd27a7337117
knixer <christopher.ono@gmail.com>
2017-02-26 16:13:16 +0100

    adding beverage.js and updating creative.js file

--- js/creative.js
+++ js/creative.js
@@ -1,5 +1,14 @@
 $(document).ready(function() {
-	$('#navBar').load("./navbar.html");
-	$('#modalLogin').load("./modal_login.html");
-	//activateTab(1);
+    $('.item-card').click(function() {
+        $('#item-collection').addClass("hidden");
+        $('#item-detailed').removeClass("hidden");
+		
+		$('.drink-description').text(bear[0].description);
+		$('.drink-name').text(bear[0].name);
+    });
+
+     $('.close-btn').click(function() {
+        $('#item-collection').removeClass("hidden");
+        $('#item-detailed').addClass("hidden");
+    });
 });
