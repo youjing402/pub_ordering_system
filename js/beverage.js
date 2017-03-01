8cd6c00db1643c59fabd8f600f104153fc6ef936
knixer <christopher.ono@gmail.com>
2017-03-01 15:26:11 +0100

    add beverage.js

---
+++ js/beverage.js
@@ -0,0 +1,10 @@
+
+
+var bear = [{
+			"name": "Passion",
+			"kCal": null,
+			"gluten": false,
+			"img": "images/A-Passion-For-Gingers,jpg",
+			"description": "Fruktig, syrlig smak med inslag av passionsfrukt, citron och ingefära. Serveras vid 8-10°C som aperitif eller som sällskapsdryck.",
+			"Volume": "330ml"
+			}]
